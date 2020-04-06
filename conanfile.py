bc1a890474b6ec4f4a2779b143714fba51ba39db
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-04-07 00:20:13 +0200

    Added conan recipe

---
+++ conanfile.py
@@ -0,0 +1,45 @@
+from conans import ConanFile, CMake, tools
+
+class TimeAdapterConan(ConanFile):
+    name = "TimeAdapter"
+    description = "C++ Time Adapter library"
+    url = "https://github.com/systelab/cpp-time-adapter"
+    homepage = "https://github.com/systelab/cpp-time-adapter"
+    author = "CSW <csw@werfen.com>"
+    topics = ("conan", "json", "configuration", "settings")
+    license = "MIT"
+    generators = "cmake_find_package"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"boost": ["1.66.0", "1.67.0", "1.71.0"], "gtest": ["1.7.0", "1.8.1", "1.10.0"]}
+    default_options = {"boost":"1.71.0", "gtest":"1.10.0"}
+    exports_sources = "*"
+
+    def configure(self):
+        self.options["TimeAdapter"].gtest = self.options.gtest
+
+    def build_requirements(self):
+        if self.options.gtest == "1.7.0":
+            self.build_requires("gtest/1.7.0@systelab/stable")
+        elif self.options.gtest == "1.8.1":
+            self.build_requires("gtest/1.8.1@bincrafters/stable")
+        else:
+            self.build_requires("gtest/1.10.0@systelab/stable")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=".")
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst=("bin/%s" % self.settings.build_type), src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy("*.so*", dst="bin", src="lib")
+
+    def package(self):
+        self.copy("TimeAdapter.h", dst="include/TimeAdapter", src="src/TimeAdapter")
+        self.copy("*TimeAdapter.lib", dst="lib", keep_path=False)
+        self.copy("*TimeAdapter.pdb", dst="lib", keep_path=False)
+        self.copy("*TimeAdapter.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
