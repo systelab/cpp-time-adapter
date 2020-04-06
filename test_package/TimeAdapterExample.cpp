b6a201090f16a556de732367bc10104ce583c575
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-04-07 01:43:34 +0200

    Improved displaying of time

--- test_package/TimeAdapterExample.cpp
+++ test_package/TimeAdapterExample.cpp
@@ -1,16 +1,16 @@
-#include <iomanip>
 #include <iostream>
-#include <limits>
-#include <map>
-#include <sstream>
-#include <stdexcept>
+#include <iomanip>
+#include <ctime>
+#include <chrono>
 
 #include "TimeAdapter/TimeAdapter.h"
 
 
 int main(int argc, char *argv[])
 {
-	auto now = systelab::time::TimeAdapter().getCurrentUTCTime();
-	std::cout << "Current UTC Time: " << now << std::endl;
+    auto now = systelab::time::TimeAdapter().getCurrentUTCTime();
+    auto now_time_t = std::chrono::system_clock::to_time_t(now);
+    std::cout << "Current UTC Time: " << std::put_time(std::localtime(&now_time_t), "%F %T") << std::endl;
+
     return 0;
 }
