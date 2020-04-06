bd40c7c1a4f568ec37bd013edb1317446733265e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-04-07 00:24:15 +0200

    Added project for automated tests

---
+++ test/TimeAdapterTest/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING 1
+
+// STL
+#include <limits>
+#include <memory>
+#include <string>
+
+// GoogleTest
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
