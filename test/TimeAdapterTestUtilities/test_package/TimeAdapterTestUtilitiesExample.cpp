90c64cafcc3b7e847b26e1ab28a07b7ec8b6dabc
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-04-07 00:25:46 +0200

    Added test for test utilities package

---
+++ test/TimeAdapterTestUtilities/test_package/TimeAdapterTestUtilitiesExample.cpp
@@ -0,0 +1,16 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING  1
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+
+#include "TimeAdapterTestUtilities/Mocks/MockTimeAdapter.h"
+
+
+int main(int argc, char *argv[])
+{
+	systelab::setting::test_utility::MockTimeAdapter timeAdapter;
+    std::cout << "TimeAdapter test utilities work as expected" << std::endl;
+
+    return 0;
+}
